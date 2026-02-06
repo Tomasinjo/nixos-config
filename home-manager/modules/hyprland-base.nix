95904e86a8a4292179fb6d182f789864b5a1eafc
Tomasinjo <tk@fikus.si>
2026-02-06 22:29:32 +0100

    fixed media keys

--- home-manager/modules/hyprland-base.nix
+++ home-manager/modules/hyprland-base.nix
@@ -100,8 +100,17 @@ let
       ",XF86Calculator, exec, rofi -show calc -modi calc -no-show-match -no-sort"
     ];
     
-    bindel = [ ",XF86AudioRaiseVolume,exec,wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+" ];
-    bindl = [ ",XF86AudioNext,exec,playerctl next" ];
+    bindel = [ 
+      ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+" 
+      ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+    ];
+    bindl = [ 
+      ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+      ", XF86AudioPlay, exec, playerctl play-pause"
+      ", XF86AudioPrev, exec, playerctl previous"
+      ", XF86AudioNext, exec, playerctl next"    
+    ];
+    
     bindm = [ "$mainMod,mouse:272,movewindow" ];
   };
 
