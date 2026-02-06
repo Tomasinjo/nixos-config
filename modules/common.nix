02cb58b09ac0355df1b30c05d313451d8e343995
Tomasinjo <tk@fikus.si>
2026-02-06 22:39:09 +0100

    added bindings for jumping and deleting words

--- modules/common.nix
+++ modules/common.nix
@@ -46,13 +46,26 @@
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
     interactiveShellInit = ''
-      bindkey -e
-      bindkey '\e[3~' delete-char 
       export HISTFILE=~/.histfile
       export HISTSIZE=1000
       export SAVEHIST=1000
       setopt HIST_IGNORE_DUPS
-    '';
+      bindkey -e
+      bindkey '\e[3~' delete-char
+
+      bindkey '^H'      backward-kill-word         # C-Backspace
+      bindkey '5~'      kill-word                  # C-Del
+      bindkey '^[[3;5~' kill-word                  # C-Del
+      bindkey '^[[3^'   kill-word                  # C-Del
+
+      bindkey '^[[1;5C' forward-word               # C-Right
+      bindkey '^[0c'    forward-word               # C-Right
+      bindkey '^[[5C'   forward-word               # C-Right
+
+      bindkey '^[[1;5D' backward-word              # C-Left
+      bindkey '^[0d'    backward-word              # C-Left
+      bindkey '^[[5D'   backward-word              # C-Left
+      '';
   };
   
   programs.starship = {
