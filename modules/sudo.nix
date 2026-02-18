d221625c32e03738d1a14c1c7b007ba2799a2dd4
Tomasinjo <tk@fikus.si>
2026-02-18 21:06:31 +0100

    added options to set EPP manually and dynamically

--- modules/sudo.nix
+++ modules/sudo.nix
@@ -7,6 +7,12 @@
     configFile = ''
       Defaults timestamp_timeout=30  # minutes until sudo timeout
     '';
+    extraRules = [{
+      users = [ "tom" ];
+      commands = [
+	{ command = "/run/current-system/sw/bin/tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference"; options = [ "NOPASSWD" ]; }  # this allows changing the file without password for user tom
+      ];
+    }];
   };
 }
 
