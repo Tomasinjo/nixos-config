5af967ffd33a177b1baa983c09988713c7ad0c5c
Tomasinjo <tk@fikus.si>
2026-03-09 09:05:02 +0100

    back to stable channel

--- flake.nix
+++ flake.nix
@@ -2,15 +2,15 @@
   description = "Zenki Home Server & Gaming Rig, Lenko Laptop";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     hyprland.url = "github:hyprwm/Hyprland";
     nixvim = {
-      url = "github:nix-community/nixvim";
+      url = "github:nix-community/nixvim/nixos-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nur.url = "github:nix-community/NUR";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
