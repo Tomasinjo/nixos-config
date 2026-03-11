209f18edded482d93c8d2dd3f45b2dd6ad97aee6
Tomasinjo <tk@fikus.si>
2026-03-12 00:50:44 +0100

    indentation fix

--- home-manager/hosts/zenki/waybar.nix
+++ home-manager/hosts/zenki/waybar.nix
@@ -3,13 +3,13 @@
   wayland.waybar = {
     extraModules = {
       temperature = {
-	align = 0;
-	justify = "left";
-	hwmon-path = "/sys/devices/platform/coretemp.0/hwmon/hwmon1/temp1_input";
-	format = " {temperatureC}°C";
-	format-critical = " {temperatureC}°C";
-	interval = 5;
-	critical-threshold = 65;
+		align = 0;
+		justify = "left";
+		hwmon-path = "/sys/devices/platform/coretemp.0/hwmon/hwmon1/temp1_input";
+		format = " {temperatureC}°C";
+		format-critical = " {temperatureC}°C";
+		interval = 5;
+		critical-threshold = 65;
       };
     };
   };
