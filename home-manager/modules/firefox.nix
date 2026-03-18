9f68376a1b838f100040efca7272c0aff521c60b
Tomasinjo <tk@fikus.si>
2026-03-18 20:51:14 +0100

    fixed missing char

--- home-manager/modules/firefox.nix
+++ home-manager/modules/firefox.nix
@@ -38,6 +38,7 @@
         "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
           installation_mode = "force_installed";
+	};
         # Dark Reader:
         "addon@darkreader.org" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
