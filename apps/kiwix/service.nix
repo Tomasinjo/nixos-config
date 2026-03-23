88f707cc42f13cb9de90d1f5a9fe447afad10170
Tomasinjo <tk@fikus.si>
2026-03-23 23:19:48 +0100

    kiwix

---
+++ apps/kiwix/service.nix
@@ -0,0 +1,33 @@
+{ lib, config, pkgs, vars, ... }:
+
+let
+  oci-framework = import ../../modules/docker/oci-framework.nix { inherit lib config vars; };
+
+  serviceName = "kiwix";
+  serviceHostname = "kb";
+  servicePort = 8080;
+
+  appContainerConfig = oci-framework.mergeAll [
+    oci-framework.base.standard
+    (oci-framework.web.internal { inherit serviceHostname servicePort; })
+    {
+      image = "ghcr.io/kiwix/kiwix-serve:3.8.2";
+
+      environment = {};
+
+      volumes = [
+        "${vars.dir.nixos_config}/apps/kiwix/app-data:/data"
+      ];
+
+      ports = [];
+      networks = [];
+      labels = {};
+      dependsOn = [];
+      
+      cmd = ["*.zim"];
+    }
+  ];
+
+in {
+  virtualisation.oci-containers.containers."${serviceName}-app" = appContainerConfig;
+}
