16aa085b16b1fbb6092dd9649addf7eecd6c4d15
Tomasinjo <tk@fikus.si>
2026-04-03 01:23:31 +0200

    fixed typos, fixed kiosk

--- modules/desktop/kiosk.nix
+++ modules/desktop/kiosk.nix
@@ -6,15 +6,35 @@
     ./fonts.nix
   ];
 
+  environment.systemPackages = [ pkgs.cage ]; 
+
   services.xserver.enable = false;  # Disable Xorg
-  programs.cage.enable = true;
 
   services.greetd = {
     enable = true;
     settings = {
       initial_session = {
-        command = "${pkgs.cage}/bin/cage -- firefox --kiosk \"https://search.${vars.net.domain}\""
+        user = vars.username; 
+        # wrap the command to ensure Wayland variables are set
+        command = pkgs.writeShellScript "kiosk-script" ''
+          export MOZ_ENABLE_WAYLAND=1
+          export XDG_SESSION_TYPE=wayland
+	  export XCURSOR_SIZE=0
+          ${pkgs.cage}/bin/cage -- firefox "https://ha.${vars.net.domain}"
+        '';
+      };
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd cage";
+        user = "greeter";
       };
     };
   };
+
+  users.users.${vars.username}.extraGroups = [ 
+    "video"
+    "input"
+  ];
+
+  # Required for Wayland/Firefox to work correctly with system d-bus
+  programs.dconf.enable = true;
 }
--- modules/ssh.nix
+++ modules/ssh.nix
@@ -8,5 +8,5 @@
       PasswordAuthentication = true;
     };
   };
-  environment.systemPackages = [ pkgs.kitty.terminfo ]  # for hosts without kitty, but with ssh: this fixes weird char echoing
+  environment.systemPackages = [ pkgs.kitty.terminfo ];  # for hosts without kitty, but with ssh: this fixes weird char echoing
 }
