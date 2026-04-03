9bdc9ca44acc4d32917228dc772ace5716c8a435
Tomasinjo <tk@fikus.si>
2026-04-03 21:59:19 +0200

    chromium policy, now works full screen in kiosk mode

--- modules/desktop/kiosk.nix
+++ modules/desktop/kiosk.nix
@@ -28,6 +28,9 @@
       "DefaultPopupsSetting" = 2;
       "BackgroundModeEnabled" = true;
       "DefaultSearchProviderEnabled" = false;
+      "FullscreenAllowed" = true;
+      "TouchVirtualKeyboardEnabled" = 1; # 1 = Always enabled
+      "KioskMode" = true;
     };
   };
 
@@ -38,10 +41,10 @@
         user = vars.username; 
         # wrap the command to ensure Wayland variables are set
         command = pkgs.writeShellScript "kiosk-script" ''
-          sleep 14  # wait for network
+          sleep 10  # wait for network
           export XDG_SESSION_TYPE=wayland
           export XCURSOR_SIZE=0
-          ${pkgs.cage}/bin/cage -- chromium --kiosk --start-fullscreen --noerrdialogs "https://ha.${vars.net.domain}"
+          ${pkgs.cage}/bin/cage -- chromium --kiosk --start-fullscreen --noerrdialogs --no-first-run --check-for-update-interval=31536000 --app="https://ha.${vars.net.domain}"
         '';
       };
       default_session = {
