affff0341aceaf79727c080e811dfc8776a219d5
Tomasinjo <tk@fikus.si>
2026-07-05 02:23:29 +0200

    updated driver to 610

--- modules/hardware/nvidia/init.nix
+++ modules/hardware/nvidia/init.nix
@@ -8,18 +8,25 @@
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
-    powerManagement.finegrained = true; # allows deep sleep states
-    open = true;
+    powerManagement.finegrained = false; # allows deep sleep states
+    open = false;
     nvidiaPersistenced = false; # if enabled, it keeps gpu alive
     #package = config.boot.kernelPackages.nvidiaPackages.latest;
     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "595.71.05";
-      sha256_64bit = "sha256-NiA7iWC35JyKQva6H1hjzeNKBek9KyS3mK8G3YRva4I=";
-      sha256_aarch64 = "sha256-Z/7IvEEa/XfZ5F5qoSIPvXJLGtscYVqjFxHZaN/M2Ts=";
-      openSha256 = "sha256-Lfz71QWKM6x/jD2B22SWpUi7/og30HRlXg1kL3EWzEw=";
-      settingsSha256 = "sha256-mXnf3jyvznfB3OfKd657rxv0rYHQb/dX/Riw/+N9EKU=";
-      persistencedSha256 = "sha256-Z/6IvEEa/XfZ5F5qoSIPvXJLGtscYVqjFxHZaN/M2Ts=";
-    };
+#      version = "595.71.05";
+#      sha256_64bit = "sha256-NiA7iWC35JyKQva6H1hjzeNKBek9KyS3mK8G3YRva4I=";
+#      sha256_aarch64 = "sha256-Z/7IvEEa/XfZ5F5qoSIPvXJLGtscYVqjFxHZaN/M2Ts=";
+#      openSha256 = "sha256-Lfz71QWKM6x/jD2B22SWpUi7/og30HRlXg1kL3EWzEw=";
+#      settingsSha256 = "sha256-mXnf3jyvznfB3OfKd657rxv0rYHQb/dX/Riw/+N9EKU=";
+#      persistencedSha256 = "sha256-Z/6IvEEa/XfZ5F5qoSIPvXJLGtscYVqjFxHZaN/M2Ts=";
+
+    version = "610.43.02";
+    sha256_64bit = "sha256-MDSgVLtM33dS/43CclZMsQVROAS/9TU4lFkBsWyndGM=";
+    sha256_aarch64 = "sha256-isWTnokUA/dzWocFBLalnk4+O5gSExVjs3dVpdYTU88=";
+    openSha256 = "sha256-hP5NVZZ4vGsACHLmUDKq4uckpd/kn1GxCSYnnJfAuBs=";
+    settingsSha256 = "sha256-0YAhufRgjDW+uR+kjaTb154fibpcDw8QowfrucoZsKE=";
+    persistencedSha256 = "sha256-Whgv9X+v2fRhzliOl2LzltY9v1SxDafFfv3IUPqj/hk=";
+};
 
     # the following allows running desktop with iGPU, while selectively offloading programs to nvidia. 
     prime = {
@@ -32,6 +39,7 @@
       nvidiaBusId = "PCI:01:00:0";
     };
   };
+
 
   hardware.nvidia-container-toolkit.enable = true;
   boot.kernelParams = [ "iomem=relaxed" ]; # for running gddr6-core-junction-vram-temps
