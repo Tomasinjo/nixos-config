e6cb1e98672947ba781b6fb8ae5b066c2454022e
Tomasinjo <tk@fikus.si>
2026-09-12 01:36:40 +0200

    clean inputs

--- helpers/get-containers.nix
+++ helpers/get-containers.nix
@@ -1,4 +1,4 @@
-{ inputs ? {}, self ? inputs.self or (throw "get-containers: 'self' argument required"), lib }:
+{ inputs, lib }:
 
 # returns attribute set of oci containers filtered by desired filter
 # default val (_: _: true) is empty expression that returns all containers
@@ -6,7 +6,7 @@
 { filter ? (_: _: true) }:
 
 let
-  zenkiConfig = self.nixosConfigurations.zenki.config;
+  zenkiConfig = inputs.self.nixosConfigurations.zenki.config;
   containers = zenkiConfig.virtualisation.oci-containers.containers;
 
 in
